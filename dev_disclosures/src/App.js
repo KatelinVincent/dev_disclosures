6b1b5650c19914da9f90f61b295f6b8c7a75de23
Katelin Vincent <95542267+KatelinVincent@users.noreply.github.com>
2024-05-14 15:55:47 +0000

    Chore : Steup plain App with inital message to users

---
+++ dev_disclosures/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p> Welcome to DevDiscolsures... a place for disclosure of knowledge, tips, and lessons learned throughout one's career. As a developer in the begining of my career, I will use this as a platform to document and practice everything I learn on my jounrney. </p> 
+        <p> More coming soon. </p>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
