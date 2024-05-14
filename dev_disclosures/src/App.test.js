f7f420d178fd3b89340d23f00dc44522f247c798
Katelin Vincent <95542267+KatelinVincent@users.noreply.github.com>
2024-05-14 19:57:02 +0000

    Chore : Basic rendering tests

--- dev_disclosures/src/App.test.js
+++ dev_disclosures/src/App.test.js
@@ -1,8 +1,30 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Post from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+describe('App component', () => {
+
+  test('App renders without crashing', () => {
+    render(<App />);
+  });
+
+  test('displays header content correctly', () => {
+    render(<App />);
+    expect(screen.getByText(/Welcome to DevDiscolsures/i)).toBeInTheDocument();
+    expect(screen.getByText(/a place for disclosure of knowledge/i)).toBeInTheDocument();
+  });
+
+  test(' Post renders without crashing', () => {
+    render(<Post />);
+  });
+
+  test('displays post content correctly', () => {
+    render(<Post />);
+    expect(screen.getByText(/Object Oriented Programming/i)).toBeInTheDocument();
+    expect(screen.getByText(/The bundling of data with the mechanisms or methods/i)).toBeInTheDocument();
+    const learnMoreLink = screen.getByText(/Learn More/i);
+    expect(learnMoreLink).toBeInTheDocument();
+  });
 });
